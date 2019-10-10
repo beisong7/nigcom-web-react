3b7d724863d582d9fbdb7e9d23ffd8dee9c43cbe
Benjamin <beisongabc@gmail.com>
2019-10-10 23:39:28 +0100

    Initial commit

---
+++ nigcom/precache-manifest.fd0878e63adc985dafaa7dd0367be45f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0517db7ce7d84610e73bba00a595c860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "171a715054e736235eb0",
+    "url": "/static/css/2.bab72b4a.chunk.css"
+  },
+  {
+    "revision": "194e72e1ad022a91c150",
+    "url": "/static/css/main.c9d85f12.chunk.css"
+  },
+  {
+    "revision": "171a715054e736235eb0",
+    "url": "/static/js/2.5f66ddef.chunk.js"
+  },
+  {
+    "revision": "194e72e1ad022a91c150",
+    "url": "/static/js/main.0ab6bbbc.chunk.js"
+  },
+  {
+    "revision": "7692b62b2b4199c70279",
+    "url": "/static/js/runtime-main.56668ad9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ac048512847ccd1542c2be81f6d9c6a0",
+    "url": "/static/media/nigcom-logo.ac048512.png"
+  },
+  {
+    "revision": "05608f44e249cfd425ad8f2cf76a327a",
+    "url": "/static/media/slider1.05608f44.jpg"
+  },
+  {
+    "revision": "98e5b2ce845beebf0428cf9ba236448a",
+    "url": "/static/media/slider2.98e5b2ce.jpg"
+  },
+  {
+    "revision": "923c5205397603a67222d02d5bc79ba4",
+    "url": "/static/media/slider3.923c5205.jpg"
+  }
+]);
